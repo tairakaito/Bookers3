26001d97c2cc7cb8321e53eb90cf4aa0bfa51c01
tairakaito <get.across.sea8@ezweb.ne.jp>
2020-02-27 09:52:23 +0000

    first

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'b4f4bb612a38b9d3b41014c705e4f89a3cd9fc9b6df7c1a0871ccad08b4948bc340e68191564653d67acc2c2b8712e674475ea98227f3f720c6038a71e6543f5'
